b7a9bf8f12e74d48fc5a321dc00a30dc6c36133e
Paul D. Karhnak <pdevkarhnak@gmail.com>
2024-08-31 07:33:30 -0400

    Sketched header file with register definitions and bit fields for MSPM0G3507.

---
+++ include/mspm0g3507.h
@@ -0,0 +1,105 @@
+/** 
+ * `msmp0g3507.h`: a header file with relevant definitions and masks for the
+ * Texas Instruments MSPM0G3507 32-bit Arm Cortex-M0+ microcontroller unit
+ * (MCU).
+ *
+ * Author: Paul D. Karhnak Developed for a capstone project since TI
+ * development tools (Code Composer Studio) apparently do not have a specific
+ * header file for the MSPM0G3507 as they do for other boards.
+ *
+ * Copyright 2024 Paul Diaz Karhnak 
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.  
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * In addition to the above terms, it should be noted that this software 
+ * product does not intend to infringe upon any applicable copyrights, patents,
+ * or intellectual property protections which are held by Texas Instruments 
+ * Incorporated ("TI"), Dallas, Texas, United States.
+ */
+
+#ifndef __MSMP0G3507_H__
+#define __MSMP0G3507_H__
+
+#include <stdint.h>
+
+/**
+ * For each registers set definition, also define bit fields.
+ */
+
+/**
+ * Interrupt assignments
+ */
+
+/**
+ * Watchdog timer registers (0, 1, ...)
+ */
+
+/**
+ * GPIO registers (0, 1/A, B, ...)
+ */
+
+/**
+ * UART registers (UART0, 1, ...)
+ */
+
+/**
+ * I2C registers (I2C0, ...)
+ */
+
+/**
+ * PWM registers (PWM0, ...)
+ */
+
+/**
+ * Timer registers (Timer0, ...)
+ */
+
+/**
+ * Timer registers (WTimer0, ...) (if applicable)
+ */
+
+/**
+ * ADC registers (ADC0, ...)
+ */
+
+/**
+ * SPI registers (SPI0, ...)
+ */
+
+/**
+ * USB registers (USB0, ...)
+ */
+
+/**
+ * System Control (SYSCTL) registers
+ */
+
+/**
+ * NVIC registers
+ */
+
+#endif // __MSPM0G3507_H__
