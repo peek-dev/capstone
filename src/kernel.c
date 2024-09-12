5d4575009999051eb89f6f33dfdca20e22b9c9e6
Paul Karhnak <zxz2hm@virginia.edu>
2024-09-12 19:59:51 -0400

    Sketched core kernel routines and implementations thereof; more work to be done with adding and removing threads.

--- src/kernel.c
+++ src/kernel.c
@@ -1,7 +1,57 @@
 #include <stdint.h>
 #include "kernel.h"
 
-uint32_t EnableInterrupts(void);
+#define THREADS_MAX 16
+tcb SystemThreads[THREADS_MAX] = {0};
+uint8_t ActiveThreads;
 
-uint3
+tcb* CurrentThread;
 
+void DisableInterrupts(void) {
+    __asm(
+            "cpsid i; \
+            bx lr;"
+         );
+}
+
+void EnableInterrupts(void) {
+    __asm(
+            "cpsie i; \
+            bx lr;"
+         );
+}
+
+void Scheduler(void) {
+    while (CurrentThread->status != THREAD_ALIVE) {
+        CurrentThread = CurrentThread->next;
+    }
+}
+
+int8_t AddThread();
+
+void ThreadYield(void) {
+    // TODO: write to SysTick to trigger a SysTick interrupt
+}
+
+void ThreadExit(void) {
+    DisableInterrupts();
+    CurrentThread->prev->next = CurrentThread->next;
+    CurrentThread->next->prev = CurrentThread->prev;
+    CurrentThread->status = THREAD_EXITED;
+    EnableInterrupts();
+    // TODO: write to SysTick to trigger a SysTick interrupt
+}
+
+void ThreadSleep(uint32_t ms) {
+    DisableInterrupts();
+    CurrentThread->sleep_ct = ms;
+    CurrentThread->status = THREAD_SLEEPING;
+    EnableInterrupts();
+    // TODO: write to SysTick to trigger a SysTick interrupt
+}
+
+void Panic(void) {
+    DisableInterrupts();
+    // TODO: potentially set LEDs to all-on or some other visual indicator
+    while (1);
+}
--- src/kernel.h
+++ src/kernel.h
@@ -3,6 +3,9 @@
 #ifndef __KERNEL_H__
 #define __KERNEL_H__
 
+#ifndef NULL
+#define NULL ((void*) 0)
+
 // Code here is inspired by/reused from Jonathan Valvano's "Embedded Systems:
 // Real Time Interfacing to ARM Cortex M Microcontrollers".
 
@@ -22,6 +25,10 @@
  http://users.ece.utexas.edu/~valvano/
  */
 
+#define THREAD_ALIVE    ((int8_t) 1)
+#define THREAD_SLEEPING ((int8_t) -1)
+#define THREAD_EXITED   ((int8_t) 0)
+
 /*
  * The thread control block struct, defining a thread's stack pointer, 
  * previous and next threads in the entire list of threads, and a sleep count
@@ -34,20 +41,81 @@ typedef struct tcb_struct {
     tcb* prev;
     tcb* next; 
     uint32_t sleep_ct;
+    int8_t status; // One of three thread statuses possible: alive (available to be scheduled), sleeping (available to be scheduled at some point in the future but currently unavailable), or dead (exited and unavailable to ever be scheduled again)
 } tcb;
 
-/**
- * Save the current mask of pending interrupts and return as a uint32_t.
- * Then, disable system interrupts.
+/** 
+ * Disable system interrupts.
  *
- * NOTE: this function is a smaller wrapper from assembly routines that was originally 
+ * NOTE: this function is a smaller wrapper from assembly routines that was
+ * originally presented in Jonathan Valvano's textbook on RTOS development for 
+ * ARM Cortex-M microcontrollers. See the copyright notice at the top of this 
+ * file for more information.
  */
-uint32_t DisableInterrupts(void);
+void DisableInterrupts(void);
 
 /**
- * Restore the mask of pending interrupts returned from a previous 
- * DisableInterrupts() call, then enable system interrupts.
+ * Enable system interrupts.
+ *
+ * NOTE: this function is a smaller wrapper from assembly routines that was
+ * originally presented in Jonathan Valvano's textbook on RTOS development for 
+ * ARM Cortex-M microcontrollers. See the copyright notice at the top of this 
+ * file for more information.
  */
-void EnableInterrupts(uint32_t InterruptMask);
+void EnableInterrupts(void);
+
+/**
+ * Schedule the next thread to run on the system.
+ *
+ * NOTE: this function should **never** be called directly; the SysTick Handler
+ * or another hardware timer interrupt should instead be left to call this 
+ * function as part of the handler execution.
+ */
+void Scheduler(void);
+
+/**
+ * Add a thread to the system's list of thread control blocks (TCBs), giving 
+ * the thread a function pointer for its relevant "main" to execute.
+ *
+ * Returns: 0 if successful or -1 if unsuccessful.
+ */
+int8_t AddThread(void (*task)(void));
+
+/**
+ * Manually trigger a context switch and run the scheduler to potentially allow
+ * another thread to run.
+ *
+ * NOTE: this function does not return in the calling thread.
+ */
+void ThreadYield(void);
+
+/**
+ * Mark the calling thread as "dead" (i.e., exited), then trigger a context 
+ * switch to allow the kernel to reclaim bookkeeping resources for this thread
+ * and possibly run another thread.
+ *
+ * NOTE: this function does not return.
+ */
+void ThreadExit(void);
+
+/**
+ * Mark the calling thread as asleep for `ms` milliseconds, then invoke a context switch to allow another thread to potentially run.
+ *
+ * NOTE: this function does not return in the caller.
+ */
+void ThreadSleep(uint32_t ms);
+
+/**
+ * A "sentinel" routine to unambiguously signal an unrecoverable machine state/
+ * fault, etc.
+ *
+ * Primarily for debugging purposes. Functions can call this to indicate a 
+ * fault condition in more broadly definitions than the onboard fault handlers
+ * allow.
+ *
+ * NOTE: this function will never return and will require a power 
+ * reset/brownout reset/etc. on the board to be exited from.
+ */
+void Panic(void);
 
 #endif /* __KERNEL_H__ */
