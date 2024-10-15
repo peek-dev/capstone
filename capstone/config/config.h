4a6fbb419ca8e6afc968f94c7b7b27f0fd3c0a26
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2024-10-15 10:58:32 -0400

    Add name for the ADC channel

---
+++ capstone/config/config.h
@@ -0,0 +1,8 @@
+#ifndef CAPSTONE_CONFIG_H
+#define CAPSTONE_CONFIG_H
+
+// For the moment, allow assertions despite the code size and memory overhead.
+#undef NDEBUG
+#include "ti_msp_dl_config.h"
+
+#endif
