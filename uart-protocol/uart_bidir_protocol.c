39022ee11db2d033617020180a290d6cfe03aaf9
Paul Karhnak <pkarhnak@gmail.com>
2024-10-17 20:24:34 -0400

    Sketched remainder of bespoke UART protocol.

--- uart-protocol/uart_bidir_protocol.c
+++ uart-protocol/uart_bidir_protocol.c
@@ -2,5 +2,129 @@
 #include <stdlib.h>
 
 /**
- * TODO: implement new API
+ * MSPM0 interface implementation
  */
+uint32_t xencode_pickup_for_rpi(CHESS_FILE src_file, CHESS_RANK src_rank, PTYPE ptype) {
+    uint32_t encoded_word = 0;
+    encoded_word |= ( ((uint32_t) src_file) << SRC_FILE_SHIFT );
+    encoded_word |= ( ((uint32_t) src_rank) << SRC_RANK_SHIFT );
+    encoded_word |= ( ((uint32_t) ptype) << PTYPE_SHIFT );
+    return encoded_word;
+}
+
+uint32_t xencode_move_for_rpi(msp_packet* move) {
+    uint32_t encoded_word = 0;
+    encoded_word |= ( ((uint32_t) move->src_file) << SRC_FILE_SHIFT );
+    encoded_word |= ( ((uint32_t) move->src_rank) << SRC_RANK_SHIFT );
+    encoded_word |= ( ((uint32_t) move->dest_file) << DEST_FILE_SHIFT );
+    encoded_word |= ( ((uint32_t) move->dest_rank) << DEST_RANK_SHIFT );
+    encoded_word |= ( ((uint32_t) move->ptype) << PTYPE_SHIFT );
+    encoded_word |= ( ((uint32_t) move->mtype) << MTYPE_SHIFT );
+    encoded_word |= ( (uint32_t) move->button_event );
+    return encoded_word;
+}
+
+uint32_t xencode_start_for_rpi(void) {
+    return 0 | BUTTON_START;
+}
+
+uint32_t xencode_restart_for_rpi(void) {
+    return 0 | BUTTON_RESTART;
+}
+
+uint32_t xencode_hint_for_rpi(CHESS_FILE src_file, CHESS_RANK src_rank) {
+    uint32_t encoded_word = 0;
+    encoded_word |= ( ((uint32_t) src_file) << SRC_FILE_SHIFT );
+    encoded_word |= ( ((uint32_t) src_rank) << SRC_RANK_SHIFT );
+    encoded_word |= BUTTON_HINT;
+    return encoded_word;
+}
+
+void vdecode_move_for_msp(uint32_t word, rpi_move* move) {
+    move->src_file = GET_SRC_FILE(word);
+    move->src_rank = GET_SRC_RANK(word);
+    move->dest_file = GET_DEST_FILE(word);
+    move->dest_rank = GET_DEST_RANK(word);
+    move->ptype = GET_PTYPE(word);
+    move->m2 = GET_M2(word);
+    move->m2_src_file = GET_M2_SRC_FILE(word);
+    move->m2_src_rank = GET_M2_SRC_RANK(word);
+    move->m2_dest_file = GET_M2_DEST_FILE(word);
+    move->m2_dest_rank = GET_M2_DEST_RANK(word);
+    move->mtype = GET_MTYPE(word);
+    move->m2_ptype = (word & 0x2) >> 1;
+    move->last = word & 0x1;
+}
+
+void vdecode_undo_for_msp(uint32_t word, rpi_undo* undo) {
+    undo->src_file = GET_SRC_FILE(word);
+    undo->src_rank = GET_SRC_RANK(word);
+    undo->dest_file = GET_DEST_FILE(word);
+    undo->dest_rank = GET_DEST_RANK(word);
+    undo->ptype = GET_PTYPE(word);
+    undo->m2 = GET_M2(word);
+    undo->m2_src_file = GET_M2_SRC_FILE(word);
+    undo->m2_src_rank = GET_M2_SRC_RANK(word);
+    undo->m2_dest_file = GET_M2_DEST_FILE(word);
+    undo->m2_dest_rank = GET_M2_DEST_RANK(word);
+    undo->bw_flag = CHECK_UNDO_BW(word);
+    undo->undone_ptype = GET_UNDO_PTYPE(word);
+}
+
+void vsend_packet_to_rpi(uint32_t word) {
+    // TODO: modify for testing to include fd/FILE*
+    return; 
+}
+
+/**
+ * RPi5 interface implementation
+ */
+uint32_t xencode_move_for_msp(rpi_move* move) {
+    uint32_t encoded_word = 0;
+    encoded_word |= ( ((uint32_t) move->src_file) << SRC_FILE_SHIFT );
+    encoded_word |= ( ((uint32_t) move->src_rank) << SRC_RANK_SHIFT );
+    encoded_word |= ( ((uint32_t) move->dest_file) << DEST_FILE_SHIFT );
+    encoded_word |= ( ((uint32_t) move->dest_rank) << DEST_RANK_SHIFT );
+    encoded_word |= ( ((uint32_t) move->ptype) << PTYPE_SHIFT );
+    encoded_word |= ( ((uint32_t) move->m2) << M2_SHIFT );
+    encoded_word |= ( ((uint32_t) move->m2_src_file) << M2_SRC_FILE_SHIFT );
+    encoded_word |= ( ((uint32_t) move->m2_src_rank) << M2_SRC_RANK_SHIFT );
+    encoded_word |= ( ((uint32_t) move->m2_dest_file) << M2_DEST_FILE_SHIFT );
+    encoded_word |= ( ((uint32_t) move->m2_dest_rank) << M2_DEST_RANK_SHIFT );
+    encoded_word |= ( ((uint32_t) move->mtype) << MTYPE_SHIFT );
+    encoded_word |= ( ((uint32_t) move->m2_ptype) << M2_PTYPE_SHIFT );
+    encoded_word |= last;
+    return encoded_word;
+}
+
+uint32_t xencode_undo_for_msp(rpi_undo* undo) {
+    uint32_t encoded_word = 0;
+    encoded_word |= ( ((uint32_t) move->src_file) << SRC_FILE_SHIFT );
+    encoded_word |= ( ((uint32_t) move->src_rank) << SRC_RANK_SHIFT );
+    encoded_word |= ( ((uint32_t) move->dest_file) << DEST_FILE_SHIFT );
+    encoded_word |= ( ((uint32_t) move->dest_rank) << DEST_RANK_SHIFT );
+    encoded_word |= ( ((uint32_t) move->ptype) << PTYPE_SHIFT );
+    encoded_word |= ( ((uint32_t) move->m2) << M2_SHIFT );
+    encoded_word |= ( ((uint32_t) move->m2_src_file) << M2_SRC_FILE_SHIFT );
+    encoded_word |= ( ((uint32_t) move->m2_src_rank) << M2_SRC_RANK_SHIFT );
+    encoded_word |= ( ((uint32_t) move->m2_dest_file) << M2_DEST_FILE_SHIFT );
+    encoded_word |= ( ((uint32_t) move->m2_dest_rank) << M2_DEST_RANK_SHIFT );
+    encoded_word |= ( ((uint32_t) move->bw_flag) << 0x3 );
+    encoded_word |= ((uint32_t) move->undone_ptype);
+    return encoded_word;
+}
+
+void vdecode_packet_for_rpi(uint32_t word, msp_packet* packet) {
+    packet->src_file = GET_SRC_FILE(word);
+    packet->src_rank = GET_SRC_RANK(word);
+    packet->dest_file = GET_DEST_FILE(word);
+    packet->dest_rank = GET_DEST_RANK(word);
+    packet->ptype = GET_PTYPE(word);
+    packet->mtype = GET_MTYPE(word);
+    packet->button_event = CHECK_BTN_EVENT(word);
+}
+
+void vsend_packet_to_msp(uint32_t word) {
+    // TODO: modify for testing to include fd/FILE*
+    return;
+}
--- uart-protocol/uart_bidir_protocol.h
+++ uart-protocol/uart_bidir_protocol.h
@@ -6,38 +6,44 @@
 /**
  * Constants for chessboard files.
  */
-#define FILE_A  ((uint32_t) 0)
-#define FILE_B  ((uint32_t) 1)
-#define FILE_C  ((uint32_t) 2)
-#define FILE_D  ((uint32_t) 3)
-#define FILE_E  ((uint32_t) 4)
-#define FILE_F  ((uint32_t) 5)
-#define FILE_G  ((uint32_t) 6)
-#define FILE_H  ((uint32_t) 7)
+typedef CHESS_FILE uint8_t;
+
+#define FILE_A  ((uint8_t) 0)
+#define FILE_B  ((uint8_t) 1)
+#define FILE_C  ((uint8_t) 2)
+#define FILE_D  ((uint8_t) 3)
+#define FILE_E  ((uint8_t) 4)
+#define FILE_F  ((uint8_t) 5)
+#define FILE_G  ((uint8_t) 6)
+#define FILE_H  ((uint8_t) 7)
 
 /**
  * Constants for chessboard ranks.
  */
-#define RANK_1  ((uint32_t) 0)
-#define RANK_2  ((uint32_t) 1)
-#define RANK_3  ((uint32_t) 2)
-#define RANK_4  ((uint32_t) 3)
-#define RANK_5  ((uint32_t) 4)
-#define RANK_6  ((uint32_t) 5)
-#define RANK_7  ((uint32_t) 6)
-#define RANK_8  ((uint32_t) 7)
+typedef CHESS_RANK uint8_t;
+
+#define RANK_1  ((uint8_t) 0)
+#define RANK_2  ((uint8_t) 1)
+#define RANK_3  ((uint8_t) 2)
+#define RANK_4  ((uint8_t) 3)
+#define RANK_5  ((uint8_t) 4)
+#define RANK_6  ((uint8_t) 5)
+#define RANK_7  ((uint8_t) 6)
+#define RANK_8  ((uint8_t) 7)
 
 /**
  * Constants for chess piece types ("ptypes")
  */
-#define PTYPE_NULL      ((uint32_t) 0x0)
-#define PTYPE_NULL_ALT  ((uint32_t) 0x4)
-#define PTYPE_PAWN      ((uint32_t) 0x1)
-#define PTYPE_KNIGHT    ((uint32_t) 0x2)
-#define PTYPE_BISHOP    ((uint32_t) 0x3)
-#define PTYPE_ROOK      ((uint32_t) 0x5)
-#define PTYPE_QUEEN     ((uint32_t) 0x6)
-#define PTYPE_KING      ((uint32_t) 0x7)
+typedef PTYPE uint8_t;
+
+#define PTYPE_NULL      ((uint8_t) 0x0)
+#define PTYPE_NULL_ALT  ((uint8_t) 0x4)
+#define PTYPE_PAWN      ((uint8_t) 0x1)
+#define PTYPE_KNIGHT    ((uint8_t) 0x2)
+#define PTYPE_BISHOP    ((uint8_t) 0x3)
+#define PTYPE_ROOK      ((uint8_t) 0x5)
+#define PTYPE_QUEEN     ((uint8_t) 0x6)
+#define PTYPE_KING      ((uint8_t) 0x7)
 
 // Friendly ptype aliases
 #define PIECE_NULL      PTYPE_NULL
@@ -52,18 +58,22 @@
 /**
  * Constants for encoded move types ("mtypes")
  */
-#define MTYPE_NORMAL            ((uint32_t) 0)
-#define MTYPE_CHECK             ((uint32_t) 1)
-#define MTYPE_CAPTURE           ((uint32_t) 2)
-#define MTYPE_CASTLE_PROMOTE    ((uint32_t) 3)
+typedef MTYPE uint8_t;
+
+#define MTYPE_NORMAL            ((uint8_t) 0)
+#define MTYPE_CHECK             ((uint8_t) 1)
+#define MTYPE_CAPTURE           ((uint8_t) 2)
+#define MTYPE_CASTLE_PROMOTE    ((uint8_t) 3)
 
 /**
  * Constants for button events
  */
-#define BUTTON_START    ((uint32_t) 0)
-#define BUTTON_RESTART  ((uint32_t) 1)
-#define BUTTON_HINT     ((uint32_t) 2)
-#define BUTTON_UNDO     ((uint32_t) 3)
+typedef BUTTON_EVENT uint8_t;
+
+#define BUTTON_START    ((uint8_t) 0)
+#define BUTTON_RESTART  ((uint8_t) 1)
+#define BUTTON_HINT     ((uint8_t) 2)
+#define BUTTON_UNDO     ((uint8_t) 3)
 
 /**
  * Constants (shifts and masks) for upper 16 bits of 32-bit UART encoded word
@@ -71,27 +81,27 @@
  */
 #define SRC_FILE_SHIFT                  29
 #define SRC_FILE_MASK                   (((uint32_t) 0x7) << SRC_FILE_SHIFT)
-#define GET_SRC_FILE((uint32_t) word)   (uint32_t) ( ((word & SRC_FILE_MASK) >> SRC_FILE_SHIFT) & 0x7 )
+#define GET_SRC_FILE((uint32_t) word)   (uint8_t) ( ((word & SRC_FILE_MASK) >> SRC_FILE_SHIFT) & 0x7 )
 
 #define SRC_RANK_SHIFT                  26
 #define SRC_RANK_MASK                   (((uint32_t) 0x7) << SRC_RANK_SHIFT)
-#define GET_SRC_RANK((uint32_t) word)   (uint32_t) ( ((word & SRC_RANK_MASK) >> SRC_RANK_SHIFT) & 0x7 )
+#define GET_SRC_RANK((uint32_t) word)   (uint8_t) ( ((word & SRC_RANK_MASK) >> SRC_RANK_SHIFT) & 0x7 )
 
 #define DEST_FILE_SHIFT                 23
 #define DEST_FILE_MASK                  (((uint32_t) 0x7) << DEST_FILE_SHIFT)
-#define GET_DEST_FILE((uint32_t) word)  (uint32_t) ( ((word & DEST_FILE_MASK) >> DEST_FILE_SHIFT) & 0x7 )
+#define GET_DEST_FILE((uint32_t) word)  (uint8_t) ( ((word & DEST_FILE_MASK) >> DEST_FILE_SHIFT) & 0x7 )
 
 #define DEST_RANK_SHIFT                 20
 #define DEST_RANK_MASK                  (((uint32_t) 0x7) << DEST_RANK_SHIFT)
-#define GET_DEST_RANK((uint32_t) word)  (uint32_t) ( ((word & DEST_RANK_MASK) >> DEST_RANK_SHIFT) & 0x7 )
+#define GET_DEST_RANK((uint32_t) word)  (uint8_t) ( ((word & DEST_RANK_MASK) >> DEST_RANK_SHIFT) & 0x7 )
 
 #define PTYPE_SHIFT                     17
 #define PTYPE_MASK                      (((uint32_t) 0x7) << PTYPE_SHIFT)
-#define GET_PTYPE((uint32_t) word)      (uint32_t) ( ((word & PTYPE_MASK) >> PTYPE_SHIFT) & 0x7 )
+#define GET_PTYPE((uint32_t) word)      (uint8_t) ( ((word & PTYPE_MASK) >> PTYPE_SHIFT) & 0x7 )
 
 #define M2_SHIFT                        16
 #define M2_MASK                         ((uint32_t) 0x1 << M2_SHIFT)
-#define GET_M2((uint32_t) word)         (uint32_t) ( ((word & M2_MASK) >> M2_SHIFT) & 0x1 )
+#define GET_M2((uint32_t) word)         (uint8_t) ( ((word & M2_MASK) >> M2_SHIFT) & 0x1 )
 
 /** 
  * Constants (shifts and masks) for parts of lower 16 bits of 32-bit UART
@@ -99,19 +109,19 @@
  */
 #define M2_SRC_FILE_SHIFT       13
 #define M2_SRC_FILE_MASK        ((uint32_t) 0x7 << M2_SRC_FILE_SHIFT)
-#define GET_M2_SRC_FILE((uint32_t) word)        (uint32_t) ( ((word & M2_SRC_FILE_MASK) >> M2_SRC_FILE_SHIFT) & 0x7)
+#define GET_M2_SRC_FILE((uint32_t) word)        (uint8_t) ( ((word & M2_SRC_FILE_MASK) >> M2_SRC_FILE_SHIFT) & 0x7)
 
 #define M2_SRC_RANK_SHIFT       10
 #define M2_SRC_RANK_MASK        ((uint32_t) 0x7 << M2_SRC_RANK_SHIFT)
-#define GET_M2_SRC_RANK((uint32_t) word)        (uint32_t) ( ((word & M2_SRC_RANK_MASK) >> M2_SRC_RANK_SHIFT) & 0x7)
+#define GET_M2_SRC_RANK((uint32_t) word)        (uint8_t) ( ((word & M2_SRC_RANK_MASK) >> M2_SRC_RANK_SHIFT) & 0x7)
 
 #define M2_DEST_FILE_SHIFT      7
 #define M2_DEST_FILE_MASK       ((uint32_t) 0x7 << M2_DEST_FILE_SHIFT)
-#define GET_M2_DEST_FILE((uint32_t) word)        (uint32_t) ( ((word & M2_DEST_FILE_MASK) >> M2_DEST_FILE_SHIFT) & 0x7)
+#define GET_M2_DEST_FILE((uint32_t) word)        (uint8_t) ( ((word & M2_DEST_FILE_MASK) >> M2_DEST_FILE_SHIFT) & 0x7)
 
 #define M2_DEST_RANK_SHIFT      4
 #define M2_DEST_RANK_MASK       ((uint32_t) 0x7 << M2_DEST_RANK_SHIFT)
-#define GET_M2_DEST_RANK((uint32_t) word)        (uint32_t) ( ((word & M2_DEST_RANK_MASK) >> M2_DEST_RANK_SHIFT) & 0x7)
+#define GET_M2_DEST_RANK((uint32_t) word)        (uint8_t) ( ((word & M2_DEST_RANK_MASK) >> M2_DEST_RANK_SHIFT) & 0x7)
 
 /** 
  * Constants (shifts and masks) and associated macros for lowest-order 4 bits
@@ -126,43 +136,88 @@
 
 #define M2_PTYPE_SHIFT                          1
 #define M2_PTYPE_MASK                           ((uint32_t) 0x2)
-#define GET_M2_PTYPE((uint32_t) word)           (uint32_t) ( ((word & M2_PTYPE_MASK) >> 1) & 0x1 )
+#define GET_M2_PTYPE((uint32_t) word)           (uint8_t) ( ((word & M2_PTYPE_MASK) >> 1) & 0x1 )
 
-#define IS_LAST_MOVE((uint32_t) word)           ((uint32_t) word & 0x1)
+#define IS_LAST_MOVE((uint32_t) word)           ((uint8_t) word & 0x1)
 
-#define CHECK_UNDO_BW((uint32_t) word)          (((uint32_t) word >> 3) & 0x1)
+#define CHECK_UNDO_BW((uint32_t) word)          (((uint8_t) word >> 3) & 0x1)
 
-#define GET_UNDO_PTYPE((uint32_t) word)         ((uint32_t) word & 0x7)
+#define GET_UNDO_PTYPE((uint32_t) word)         ((uint8_t) word & 0x7)
 
-#define CHECK_BTN_EVENT((uint32_t) word)        ((uint32_t) word & 0x3)
+#define CHECK_BTN_EVENT((uint32_t) word)        ((uint8_t) word & 0x3)
 
 /**
  * Encoding and decoding interface for MSPM0
  */
 typedef struct msp_move_packet {
-    uint32_t src_file;
-    uint32_t src_rank;
-    uint32_t dest_file;
-    uint32_t dest_rank;
-    uint32_t ptype;    
-    uint32_t mtype;
-    uint32_t button_event;
+    CHESS_FILE src_file;
+    CHESS_RANK src_rank;
+    CHESS_FILE dest_file;
+    CHESS_RANK dest_rank;
+    PTYPE ptype;    
+    MTYPE mtype;
+    BUTTON_EVENT button_event;
 } msp_packet;
 
-uint32_t xencode_pickup_for_rpi(uint32_t src_file, uint32_t src_rank, uint32_t ptype);
+typedef struct rpi_move_packet {
+    CHESS_FILE src_file;
+    CHESS_RANK src_rank;
+    CHESS_FILE dest_file;
+    CHESS_RANK dest_rank;
+    PTYPE ptype;
+    uint8_t m2;
+    CHESS_FILE m2_src_file;
+    CHESS_RANK m2_src_rank;
+    CHESS_FILE m2_dest_file;
+    CHESS_RANK m2_dest_rank;
+    MTYPE mtype;
+    uint8_t m2_ptype;
+    uint8_t last;
+} rpi_move;
 
-uint32_t xencode_move_for_rpi(msp_packet* validated_move_packet);
+typedef struct rpi_undo_packet {
+    CHESS_FILE src_file;
+    CHESS_RANK src_rank;
+    CHESS_FILE dest_file;
+    CHESS_RANK dest_rank;
+    PTYPE ptype;
+    uint8_t m2;
+    CHESS_FILE m2_src_file;
+    CHESS_RANK m2_src_rank;
+    CHESS_FILE m2_dest_file;
+    CHESS_RANK m2_dest_rank;
+    uint8_t bw_flag;
+    PTYPE undone_ptype;
+} rpi_undo;
+
+uint32_t xencode_pickup_for_rpi(CHESS_FILE src_file, CHESS_RANK src_rank, PTYPE ptype);
+
+uint32_t xencode_move_for_rpi(msp_packet* move);
 
 uint32_t xencode_start_for_rpi(void);
+
 uint32_t xencode_restart_for_rpi(void);
 
-uint32_t xencode_hint_for_rpi(uint32_t src_file, uint32_t src_rank, uint32_t hint_event);
+uint32_t xencode_hint_for_rpi(CHESS_FILE src_file, CHESS_RANK src_rank);
 
-uint32_t xencode_undo_for_rpi(uint32_t undo_event);
+uint32_t xencode_undo_for_rpi(BUTTON_EVENT undo_event);
+
+void vdecode_move_for_msp(uint32_t word, rpi_move* move);
+
+void vdecode_undo_for_msp(uint32_t word, rpi_undo* undo);
+
+void vsend_packet_to_rpi(uint32_t word);
 
 /**
  * Encoding and decoding interface for RPi5
- * TODO: add more
  */
+
+uint32_t xencode_move_for_msp(rpi_move* move);
+
+uint32_t xencode_undo_for_msp(rpi_undo* undo);
+
+void vdecode_packet_for_rpi(uint32_t word, msp_packet* packet);
+
+void vsend_packet_to_msp(uint32_t word);
 
 #endif
