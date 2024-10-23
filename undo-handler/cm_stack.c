e04f2294dc86aecf92f53d612c20242a347aafb6
Paul D. Karhnak <pdevkarhnak@gmail.com>
2024-10-23 10:38:00 -0400

    Sketched chessmove stack implementation, including private helper function.

---
+++ undo-handler/cm_stack.c
@@ -0,0 +1,75 @@
+#include <stdlib.h>
+#include "cm_stack.h"
+
+cm_stack_t* cm_stack_init(void) {
+    cm_stack_t* new_stack = (cm_stack_t*) malloc(1 * sizeof(cm_stack_t));
+
+    if (new_stack == NULL) {
+        return NULL;
+    }
+
+    new_stack->top = NULL;
+    new_stack->nodecount = 0;
+
+    return new_stack;
+}
+
+static cm_node_t* cm_node_init(chessmove_t move) {
+    cm_node_t* new_node = (cm_node_t*) malloc(1 * sizeof(cm_node_t));
+
+    if (new_node == NULL) {
+        return NULL;
+    }
+
+    new_node->move = move;
+    new_node->next = NULL;
+
+    return new_node;
+}
+
+void cm_stack_push(cm_stack_t* stack, chessmove_t new_move) {
+    cm_node_t* new_node = cm_node_init(new_move);
+
+    if (new_node == NULL) {
+        return;
+    }
+
+    new_node->next = stack->top;
+    stack->top = new_node;
+    stack->nodecount += 1;
+}
+
+uint32_t cm_stack_pop(cm_stack_t* stack) {
+    if (stack->top == NULL) {
+        return 0xFFFFFFFF;
+    }
+
+    cm_node_t* popped_node = stack->top;
+    stack->top = stack->top->next;
+    stack->nodecount -= 1;
+
+    uint32_t popped_data = popped_node->move;
+    free(popped_node);
+
+    return popped_data;
+}
+
+void cm_stack_destroy(cm_stack* stack) {
+    stack->nodecount = 0;
+
+    if (stack->top == NULL) {
+        free(stack);
+        return;
+    }
+
+    cm_node_t* destroyed_node = stack->top;
+    cm_node_t* next_node;
+
+    do {
+        next_node = destroyed_node->next;
+        free(destroyed_node);
+        destroyed_node = next_node;
+    } while (destroyed_node != NULL);
+
+    free(stack);
+}
--- undo-handler/cm_stack.h
+++ undo-handler/cm_stack.h
@@ -1,6 +1,8 @@
 #ifndef __CM_STACK_H__
 #define __CM_STACK_H__
 
+#include <stdint.h>
+
 /**
  * A friendly, clearer alias.
  */
@@ -29,5 +31,28 @@ typedef struct cm_stack_t_struct cm_stack_t {
     cm_node_t* top;
     int nodecount;
 } cm_stack_t;
+
+/**
+ * Initialize a chessmove stack on the heap and return a pointer to a valid 
+ * stack.
+ */
+cm_stack_t* cm_stack_init(void);
+
+/**
+ * Push a 32-bit word to a stack, invoking necessary internal logic to wrap the
+ * data in a node and adjust stack state.
+ */
+void cm_stack_push(cm_stack_t* stack, chessmove_t new_move);
+
+/**
+ * Pop the top node from a stack and return the data from that node as a 
+ * uint32_t (the type that "chessmove_t" aliases).
+ */
+uint32_t cm_stack_pop(cm_stack_t* stack);
+
+/**
+ * Clean up and free a stack and its associated heap space.
+ */
+void cm_stack_destroy(cm_stack* stack);
 
 #endif
