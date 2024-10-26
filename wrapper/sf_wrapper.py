4385f5560516353728fccac43d984e478ec1666d
Paul D. Karhnak <pdevkarhnak@gmail.com>
2024-10-26 15:59:27 -0400

    Sketched out the remainder of the Stockfish wrapper per-turn logic.

--- wrapper/sf_wrapper.py
+++ wrapper/sf_wrapper.py
@@ -4,18 +4,32 @@ import chess
 import chess.engine
 from enum import Enum
 
-class Event(Enum):
-    NORMAL = 0,
+
+class ButtonEvent(Enum):
+    NORMAL = 0
+    RESTART = 1
+    HINT = 2
+    UNDO = 3
+
 
 # TODO: build C-based UART extension module to enable importing here
-# import rpi5_uartboost
+# import rpi5_uartboost as uart
+
+def parse_button_event(packet: int) -> ButtonEvent:
+    match (packet & 0x3):
+        case 0x1:
+            return ButtonEvent.RESTART
+        case 0x2:
+            return ButtonEvent.HINT
+        case 0x3:
+            return ButtonEvent.UNDO
+        case _:
+            return ButtonEvent.NORMAL
+
 
 def send_move(move: chess.Move, is_last_move: bool=False) -> None:
     return
 
-
-def get_next_move() -> chess.Move:
-    return
 
 if __name __ == '__main__':
 
@@ -31,22 +45,47 @@ if __name __ == '__main__':
     sf.configure({'Hash': 4096})
     sf.configure({'Threads': 4})
 
+    # A limit of 250 ms for Stockfish's per-turn compute time, wrapped in the
+    # python-chess API.
+    sf_limit = chess.engine.Limit(time=0.250)
+
     while True:
-        # Process main loop
+        next_result = sf.play(board, sf_limit)
+        best_move = next_result.move
+        possible_moves = board.legal_moves
 
-        # 1. Fetch next packet from UART and decode
+        # TODO: send best move "a priori" either at very beginning 
+        # or very end (?)
+        for move in possible_moves:
+            if (move == best_move):
+                # Special logic: skip?
+                continue
 
-        # IF undo event:
-        # undone_move = `board.pop()` or similar
-        # Catch undo-on-empty-stack condition (IndexError)
-        # if undone_move exists (i.e., IndexError not raised), encode undone move
-        # and transmit to MSP over UART
-        # else, encode NULL packet and send (?)
+            # TODO: send packet
 
-        # 1a. Validate move (???)
+        # TODO: receive next packet from MSP
 
-        # 2. push move to board stack: `board.push_uci()` or similar
-        pass
+        next_move = chess.Move.null()
+
+        match (parse_button_event(next_packet)):
+            case ButtonEvent.RESTART:
+                board.clear()
+                continue
+            case ButtonEvent.HINT:
+                # TODO: send best move as packet again?
+            case ButtonEvent.UNDO:
+                undone_move = board.pop()
+                # TODO: encode and send undo-formatted packet
+                continue
+            case _:
+                next_move = # TODO: assign to result of packet decoding + chess.Move.from_uci(decoded_string)
+                pass # No extra logic needed for standard move
+
+        board.push_uci(next_move)
+
+        # TODO: check post-move conditions: check, checkmate, stalemate
+
+        # TODO: send specially-formatted packets to correspond to endgame conditions
 
     sf.quit()
 
