5106724b5c7cc6dc1342fef0a6f75d6cd1ad6e85
Paul D. Karhnak <pdevkarhnak@gmail.com>
2024-10-26 15:09:21 -0400

    Restructured and slightly expanded logic for wrapper; introduced prototypes for portions of encoding/decoding stack.

--- wrapper/sf_wrapper.py
+++ wrapper/sf_wrapper.py
@@ -2,34 +2,52 @@
 
 import chess
 import chess.engine
+from enum import Enum
 
-board = chess.Board()
+class Event(Enum):
+    NORMAL = 0,
 
-# Open Stockfish in subprocess.
-# NOTE: on this build of Raspberry Pi OS/Debian, Stockfish is installed at
-# /usr/games, NOT /usr/bin
-sf = chess.engine.SimpleEngine.popen_uci("/usr/games/stockfish")
+# TODO: build C-based UART extension module to enable importing here
+# import rpi5_uartboost
 
-# Provision 4 threads and 4096 MB (4 GB) of RAM to Stockfish. Raspberry Pi 5's 
-# ARM Cortex-A76 has 4 cores, and the targeted RPi5 has 8GB RAM available.
-sf.configure({'Hash': 4096})
-sf.configure({'Threads': 4})
+def send_move(move: chess.Move, is_last_move: bool=False) -> None:
+    return
 
-while True:
-    # Process main loop
 
-    # 1. Fetch next packet from UART and decode
+def get_next_move() -> chess.Move:
+    return
 
-    # IF undo event:
-    # undone_move = `board.pop()` or similar
-    # Catch undo-on-empty-stack condition (IndexError)
-    # if undone_move exists (i.e., IndexError not raised), encode undone move
-    # and transmit to MSP over UART
-    # else, encode NULL packet and send (?)
+if __name __ == '__main__':
 
-    # 1a. Validate move (???)
+    board = chess.Board()
 
-    # 2. push move to board stack: `board.push_uci()` or similar
-    pass
+    # Open Stockfish in subprocess.
+    # NOTE: on this build of Raspberry Pi OS/Debian, Stockfish is installed at
+    # /usr/games, NOT /usr/bin
+    sf = chess.engine.SimpleEngine.popen_uci("/usr/games/stockfish")
 
-sf.quit()
+    # Provision 4 threads and 4096 MB (4 GB) of RAM to Stockfish. Raspberry Pi 5's 
+    # ARM Cortex-A76 has 4 cores, and the targeted RPi5 has 8GB RAM available.
+    sf.configure({'Hash': 4096})
+    sf.configure({'Threads': 4})
+
+    while True:
+        # Process main loop
+
+        # 1. Fetch next packet from UART and decode
+
+        # IF undo event:
+        # undone_move = `board.pop()` or similar
+        # Catch undo-on-empty-stack condition (IndexError)
+        # if undone_move exists (i.e., IndexError not raised), encode undone move
+        # and transmit to MSP over UART
+        # else, encode NULL packet and send (?)
+
+        # 1a. Validate move (???)
+
+        # 2. push move to board stack: `board.push_uci()` or similar
+        pass
+
+    sf.quit()
+
+
