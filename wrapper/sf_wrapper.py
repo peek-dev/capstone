35e52205a27410bc3b1038b4692f7f58622253dd
Paul D. Karhnak <paulk@virginia.edu>
2024-11-14 14:58:19 -0500

    Swapped out bespoke UART booster invocations with pyserial invocations.

--- wrapper/sf_wrapper.py
+++ wrapper/sf_wrapper.py
@@ -4,6 +4,7 @@ import chess
 import chess.engine
 import wrapper_util as wr # include helpers and UART protocol constants
 import rpi_uartboost as uart # include bespoke Raspberry Pi UART booster library
+import serial
 
 if __name__ == '__main__':
     board = chess.Board()
@@ -22,8 +23,7 @@ if __name__ == '__main__':
     # 250ms per-turn computation time limit
     sf_limit = chess.engine.Limit(time=0.250)
 
-    # Cue extension libary to prepare UART for reading and writing
-    uart.uart_init()
+    uart = serial.Serial('/dev/serial0', baudrate=921600)
 
     # The main loop of the program, which will never exit except in unusual 
     # circumstances.
@@ -52,7 +52,7 @@ if __name__ == '__main__':
             if move_src_square not in moves_dict.keys():
                 moves_dict[move_src_square] = []
 
-            moves_dict[move_src_square].append(wr.encode_packet(move, board))
+            moves_dict[move_src_square].append(wr.encode_packet(move, board).to_bytes(4, 'little'))
 
         packet_no = 0
 
@@ -63,13 +63,14 @@ if __name__ == '__main__':
                 # Reshuffle makes pinpointing last move trickier than simple 
                 # is-move-last-in-list logic
                 if packet_no == possible_count:
-                    uart.uart_sendpacket(packet | 0x1)
+                    packet = (int(packet.hex(), 16) | 0x01000000).to_bytes(4, 'little')
+                    uart.write(packet)
                 else:
-                    uart.uart_sendpacket(packet)
+                    uart.write(packet)
 
         # Block until the MSP sends back the next event it receives on the 
         # chessboard.
-        next_packet = uart.uart_recvpacket()
+        next_packet = uart.read(4)
         next_move = chess.Move.null()
 
         # Three distinct possible kinds of packets (in a sense) exist to prompt
@@ -77,34 +78,34 @@ if __name__ == '__main__':
         # - RESTART: clear game state (rewind entire game)
         # - HINT: send best move
         # - UNDO: rewind game state one move at a time
-        match (wr.parse_button_event(next_packet)):
+        match (wr.parse_button_event(int(next_packet.hex()))):
             case wr.ButtonEvent.RESTART:
                 board.clear()
                 continue
             case wr.ButtonEvent.HINT:
-                uart.uart_sendpacket(wr.encode_packet(best_move, board, True))
+                uart.uart_sendpacket(wr.encode_packet(best_move, board, True).to_bytes(4, 'little'))
             case wr.ButtonEvent.UNDO:
                 try:
                     # Rewinds the board state---no further action needed.
                     undone_move = board.pop()
                     # Indicate to the MSP which specific move has been undone
-                    uart.uart_sendpacket(wr.encode_undo(undone_move, board))
+                    uart.uart_sendpacket(wr.encode_undo(undone_move, board).to_bytes(4, 'little'))
                     continue
                 except IndexError:
                     # IndexError innocuous; stack is empty, but not an error
                     continue 
             case _:
                 # Standard move
-                next_move = wr.decode_packet(next_packet) 
+                next_move = wr.decode_packet(int(next_packet.hex(), 16)) 
 
         # Add move to the playing stack for Stockfish to use
         board.push(next_move)
 
         # Check endgame conditions and indicate to MSP if present
         if (board.is_checkmate()):
-            uart.uart_sendpacket(wr.CHECKMATE)
+            uart.uart_sendpacket(wr.CHECKMATE.to_bytes(4, 'little'))
         elif (board.is_stalemate()):
-            uart.uart_sendpacket(wr.STALEMATE)
+            uart.uart_sendpacket(wr.STALEMATE.to_bytes(4, 'little'))
 
         # ...then proceed to the next turn
 
