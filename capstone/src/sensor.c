a4c97d0d6cafaf11dfdd5b42c7585904364501e9
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2024-11-23 17:46:34 -0500

    Enable interrupt in NVIC. Adjust timing, bins. Fix row/col gpio mixup.

--- capstone/src/sensor.c
+++ capstone/src/sensor.c
@@ -1,4 +1,5 @@
 #include "config.h"
+#include "portmacro.h"
 #include "task.h"
 #include "assert.h"
 #include "ti/driverlib/dl_adc12.h"
@@ -7,20 +8,19 @@
 
 #include "game.h"
 #include "sensor.h"
+#include "main.h"
+#include "ti_msp_dl_config.h"
 
 #define NBINS 13
 // Based on our filter step responses, we should wait 150us for a column switch.
 // (150 us * 32 MHz) - 1
-#define COL_SWITCH_LOAD (150 * 32 - 1)
+#define COL_SWITCH_LOAD (150U * 64U / 10U - 1U)
 // Similarly, 50us for a row switch.
-#define ROW_SWITCH_LOAD (50 * 32 - 1)
+#define ROW_SWITCH_LOAD (50U * 64U / 10U - 1U)
 
 // For the ADC: ADC samples are asynchronous, so we need to wakeup the task from
 // an ISR.
 static TaskHandle_t xSensorTaskId = NULL;
-
-// Function to call when we have just finished a board.
-static BaseType_t (*prvBoardUpdate)(BoardState *) = NULL;
 
 // Lower bounds for hall effect sensor reading bins.
 // Generate this with python (numpy):
@@ -28,8 +28,7 @@ static BaseType_t (*prvBoardUpdate)(BoardState *) = NULL;
 // 0.95, 1.1, 1.3, 1.55, 1.9, 2.1, 2.3, 2.4, 2.8, 3.0])/3.3).astype(int) OPT:
 // make this packed 12-bit?
 // TODO: calibration?
-static uint16_t bins[] = {0,    248,  745,  1179, 1365, 1613, 1923,
-                          2358, 2606, 2854, 2978, 3475, 3723};
+static uint16_t bins[] = {0, 173, 567, 962, 1332, 1708, 1968, 2128, 2387, 2763, 3133, 3528, 3923};
 
 static PieceType prvValueToPiece(uint16_t value) {
     uint8_t i;
@@ -44,36 +43,35 @@ static PieceType prvValueToPiece(uint16_t value) {
 
 static void prvSelectRow(uint8_t row) {
     assert(row < 8);
-    DL_GPIO_writePinsVal(MUX_GPIO_PIN_C_A0_PORT, MUX_GPIO_PIN_C_A0_PIN,
-                         MUX_GPIO_PIN_C_A0_PIN * !!(row & 1));
-    DL_GPIO_writePinsVal(MUX_GPIO_PIN_C_A1_PORT, MUX_GPIO_PIN_C_A1_PIN,
-                         MUX_GPIO_PIN_C_A1_PIN * !!(row & 2));
-    DL_GPIO_writePinsVal(MUX_GPIO_PIN_C_A2_PORT, MUX_GPIO_PIN_C_A2_PIN,
-                         MUX_GPIO_PIN_C_A2_PIN * !!(row & 4));
+    DL_GPIO_writePinsVal(MUX_GPIO_PIN_R_A0_PORT, MUX_GPIO_PIN_R_A0_PIN,
+                         MUX_GPIO_PIN_R_A0_PIN * !!(row & 1));
+    DL_GPIO_writePinsVal(MUX_GPIO_PIN_R_A1_PORT, MUX_GPIO_PIN_R_A1_PIN,
+                         MUX_GPIO_PIN_R_A1_PIN * !!(row & 2));
+    DL_GPIO_writePinsVal(MUX_GPIO_PIN_R_A2_PORT, MUX_GPIO_PIN_R_A2_PIN,
+                         MUX_GPIO_PIN_R_A2_PIN * !!(row & 4));
 }
 
 static void prvSelectColumn(uint8_t column) {
     assert(column < 8);
-    DL_GPIO_writePinsVal(MUX_GPIO_PIN_R_A0_PORT, MUX_GPIO_PIN_R_A0_PIN,
-                         MUX_GPIO_PIN_R_A0_PIN *
+    DL_GPIO_writePinsVal(MUX_GPIO_PIN_C_A0_PORT, MUX_GPIO_PIN_C_A0_PIN,
+                         MUX_GPIO_PIN_C_A0_PIN *
                              (!!(column & 1) ^ !!(column & 4)));
-    DL_GPIO_writePinsVal(MUX_GPIO_PIN_R_A1_PORT, MUX_GPIO_PIN_R_A1_PIN,
-                         MUX_GPIO_PIN_R_A1_PIN *
+    DL_GPIO_writePinsVal(MUX_GPIO_PIN_C_A1_PORT, MUX_GPIO_PIN_C_A1_PIN,
+                         MUX_GPIO_PIN_C_A1_PIN *
                              (!!(column & 2) ^ !!(column & 4)));
-    DL_GPIO_writePinsVal(MUX_GPIO_PIN_R_A2_PORT, MUX_GPIO_PIN_R_A2_PIN,
-                         MUX_GPIO_PIN_R_A2_PIN * !!(column & 4));
+    DL_GPIO_writePinsVal(MUX_GPIO_PIN_C_A2_PORT, MUX_GPIO_PIN_C_A2_PIN,
+                         MUX_GPIO_PIN_C_A2_PIN * !!(column & 4));
 }
 
 static void prvSingleADC(BoardState *board, uint8_t row, uint8_t column) {
-
     DL_ADC12_startConversion(ADC_0_INST);
-
     // Block the thread until ADC sampling is complete.
-    ulTaskNotifyTake(pdTRUE, pdMS_TO_TICKS(10));
+    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 
-    uint16_t sample =
+    volatile uint16_t sample =
         DL_ADC12_getMemResult(ADC_0_INST, ADC_0_ADCMEM_ChessSquare);
     vSetSquare(board, row, column, prvValueToPiece(sample));
+    DL_ADC12_enableConversions(ADC_0_INST);
 }
 
 void ADC_0_INST_IRQHandler(void) {
@@ -111,6 +109,10 @@ void SENSOR_DELAY_TIMER_INST_IRQHandler(void) {
 BaseType_t xSensor_Init(void) {
     prvSelectColumn(0);
     prvSelectRow(0);
+    NVIC_ClearPendingIRQ(SENSOR_DELAY_TIMER_INST_INT_IRQN);
+    NVIC_EnableIRQ(SENSOR_DELAY_TIMER_INST_INT_IRQN);
+    NVIC_ClearPendingIRQ(ADC_0_INST_INT_IRQN);
+    NVIC_EnableIRQ(ADC_0_INST_INT_IRQN);
     return pdTRUE;
 }
 
@@ -141,6 +143,6 @@ void vSensor_Thread(void *arg0) {
                 prvSingleADC(&board, row, col);
             }
         }
-        prvBoardUpdate(&board);
+        xMain_sensor_update(&board);
     }
 }
