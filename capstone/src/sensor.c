ad64c7de1aac4f552aa059d31abe036cabc0f587
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2024-12-02 22:26:06 -0500

    Fix mux pins: R=row, C=central. Previous time was wrong.

--- capstone/src/sensor.c
+++ capstone/src/sensor.c
@@ -29,8 +29,8 @@ static TaskHandle_t xSensorTaskId = NULL;
 // 0.95, 1.1, 1.3, 1.55, 1.9, 2.1, 2.3, 2.4, 2.8, 3.0])/3.3).astype(int) OPT:
 // make this packed 12-bit?
 // TODO: calibration?
-static uint16_t bins[] = {0,    173,  567,  962,  1332, 1708, 1968,
-                          2128, 2387, 2763, 3133, 3528, 3923};
+static uint16_t bins[] = {0,  249,  746, 1118, 1402, 1626, 1985, 2097, 2234, 2569, 2792,
+       3164, 3723};
 
 static PieceType prvValueToPiece(uint16_t value) {
     uint8_t i;
@@ -47,24 +47,24 @@ static void prvSelectRow(uint8_t row) {
     assert(row < 8);
     // Hack: all rows were reversed.
     row = 7 - row;
-    DL_GPIO_writePinsVal(MUX_GPIO_PIN_R_A0_PORT, MUX_GPIO_PIN_R_A0_PIN,
-                         MUX_GPIO_PIN_R_A0_PIN * !!(row & 1));
-    DL_GPIO_writePinsVal(MUX_GPIO_PIN_R_A1_PORT, MUX_GPIO_PIN_R_A1_PIN,
-                         MUX_GPIO_PIN_R_A1_PIN * !!(row & 2));
-    DL_GPIO_writePinsVal(MUX_GPIO_PIN_R_A2_PORT, MUX_GPIO_PIN_R_A2_PIN,
-                         MUX_GPIO_PIN_R_A2_PIN * !!(row & 4));
+    DL_GPIO_writePinsVal(MUX_GPIO_PIN_C_A0_PORT, MUX_GPIO_PIN_C_A0_PIN,
+                         MUX_GPIO_PIN_C_A0_PIN * !!(row & 1));
+    DL_GPIO_writePinsVal(MUX_GPIO_PIN_C_A1_PORT, MUX_GPIO_PIN_C_A1_PIN,
+                         MUX_GPIO_PIN_C_A1_PIN * !!(row & 2));
+    DL_GPIO_writePinsVal(MUX_GPIO_PIN_C_A2_PORT, MUX_GPIO_PIN_C_A2_PIN,
+                         MUX_GPIO_PIN_C_A2_PIN * !!(row & 4));
 }
 
 static void prvSelectColumn(uint8_t column) {
     assert(column < 8);
-    DL_GPIO_writePinsVal(MUX_GPIO_PIN_C_A0_PORT, MUX_GPIO_PIN_C_A0_PIN,
-                         MUX_GPIO_PIN_C_A0_PIN *
+    DL_GPIO_writePinsVal(MUX_GPIO_PIN_R_A0_PORT, MUX_GPIO_PIN_R_A0_PIN,
+                         MUX_GPIO_PIN_R_A0_PIN *
                              (!!(column & 1) ^ !!(column & 4)));
-    DL_GPIO_writePinsVal(MUX_GPIO_PIN_C_A1_PORT, MUX_GPIO_PIN_C_A1_PIN,
-                         MUX_GPIO_PIN_C_A1_PIN *
+    DL_GPIO_writePinsVal(MUX_GPIO_PIN_R_A1_PORT, MUX_GPIO_PIN_R_A1_PIN,
+                         MUX_GPIO_PIN_R_A1_PIN *
                              (!!(column & 2) ^ !!(column & 4)));
-    DL_GPIO_writePinsVal(MUX_GPIO_PIN_C_A2_PORT, MUX_GPIO_PIN_C_A2_PIN,
-                         MUX_GPIO_PIN_C_A2_PIN * !!(column & 4));
+    DL_GPIO_writePinsVal(MUX_GPIO_PIN_R_A2_PORT, MUX_GPIO_PIN_R_A2_PIN,
+                         MUX_GPIO_PIN_R_A2_PIN * !!(column & 4));
 }
 
 static void prvSingleADC(BoardState *board, uint8_t row, uint8_t column) {
