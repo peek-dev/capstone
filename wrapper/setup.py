6de5d3530031d18dc53e83d4ca328e61517bae99
Paul D. Karhnak <pdevkarhnak@gmail.com>
2024-12-07 13:52:40 -0500

    Deleted unnecessary setup.py now that code no longer relies on bespoke UART module.

--- wrapper/setup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from distutils.core import setup, Extension
-
-module = Extension("rpi_uartboost", sources = ["rpi_uartboost.c"])
-
-setup(name='PackageName', version='0.40', description='Booster module to interact with file-mapped UART on Raspberry Pi OS.', ext_modules = [module])
