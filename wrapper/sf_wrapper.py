f2b98be4d824746b46ff1ee4c72218b508de7225
Paul Karhnak <pkarhnak@gmail.com>
2024-12-19 12:43:25 -0500

    Messily spliced in the wrapper core files to main.

---
+++ wrapper/sf_wrapper.py
@@ -0,0 +1,278 @@
+#!/usr/bin/env python3
+
+import chess
+import chess.engine
+import wrapper_util as wr # include helpers and UART protocol constants
+from wrapper_util import ButtonEvent
+import serial
+from datetime import datetime
+import sys  # for basic argparsing: argv[1] 
+import time
+
+
+def push_msg(msg: str):
+    print("[DEBUG]:", msg)
+
+
+if __name__ == '__main__':
+    debug = None
+
+    try:
+        if (sys.argv[1] == '-s') or ('-suppress' in sys.argv[1]):
+            debug = False
+        else:
+            debug = True
+    except IndexError:
+        debug = True
+
+    if debug:
+        push_msg(f"STARTED at: {datetime.now().isoformat()}")
+
+    MSP_BAUDRATE=115200 # UART state constant
+    board = chess.Board()
+
+    # Open Stockfish in subprocess.
+    # NOTE: on this build of Raspberry Pi OS/Debian, Stockfish is installed at
+    # /usr/games/, NOT /usr/bin/
+    sf = chess.engine.SimpleEngine.popen_uci("/usr/games/stockfish")
+
+    if debug:
+        push_msg("Opened stockfish subprocess.")
+
+    # Device-specific customizations.
+    # Code originally developed for Raspberry Pi 4B with 4 cores and 8GB RAM
+    # (hence 'Hash' --> memory allocation --> 4096 MiB = 4 GiB).
+    sf.configure({'Hash': 4096})
+    sf.configure({'Threads': 4})
+
+    # 250ms per-turn computation time limit
+    sf_limit = chess.engine.Limit(time=0.250)
+
+    if debug:
+        push_msg("Configured stockfish (memory, threads, time limit)")
+
+    uart = serial.Serial('/dev/serial0', baudrate=MSP_BAUDRATE)
+
+    msp_alive = True
+
+    while True:
+
+        if debug:
+            push_msg("Waiting for heartbeat...")
+
+        # "Heartbeat code" to establish connection with MSP
+        heartbeat_msg = uart.read_until(wr.MSP_SYN.to_bytes(4, 'little'))
+
+        if debug:
+            push_msg(f"Got heartbeat message: {heartbeat_msg}")
+
+        heartbeat = int(heartbeat_msg[-1::-1].hex(), 16)
+
+        if debug:
+            push_msg(f"Received heartbeat: {hex(heartbeat)}")
+
+        uart.write(wr.SYNACK.to_bytes(4, 'little'))
+        uart.write(wr.SYNACK.to_bytes(4, 'little'))
+
+        if debug:
+            push_msg(f"Sent SYNACK: {hex(wr.SYNACK)}")
+
+        uart.read_until(wr.MSP_ACK.to_bytes(4, 'little'))
+             
+        if debug:
+            push_msg(f"Received ACK ({hex(wr.MSP_ACK)}) from MSP.")
+
+        # Now synchronized with MSP. Proceed with setup.
+        wr.init_board(board, uart, log=debug)
+
+        if debug:
+            push_msg("Initialized board.")
+
+        next_result = None
+
+        while (next_result == None):
+            try:
+                next_result = sf.play(board, sf_limit)
+            except TimeoutError:
+                pass
+
+        best_move = next_result.move
+
+        if debug:
+            push_msg(f"Got initial best move: {best_move.uci()}")
+
+        current_timestamp = datetime.now().isoformat()
+        current_timestamp = current_timestamp.replace(':', '-')
+        data_handle = f"/opt/chessboard/{current_timestamp}-game.txt"
+    
+        # The main loop of the program, which will never exit except in unusual 
+        # circumstances.
+        while msp_alive:
+            if debug:
+                push_msg("Waiting for next packet...")
+
+            # Block until the MSP sends back the next event it receives on the 
+            # chessboard.
+            next_packet = int((uart.read(4))[-1::-1].hex(), 16)
+
+            if debug:
+                push_msg(f"Got packet: {hex(next_packet)}")
+
+            # Sentinel move/dummy move ("done undoing"). No useful information encoded here, so continue.
+            if (next_packet == wr.MSP_SYN):
+                push_msg("Packet indicates MSP reset! Resetting state...")
+                msp_alive = False
+                continue
+            if (next_packet == wr.MSP_REREQ):
+                if debug:
+                    push_msg("Packet is sentinel move/dummy move (\"done undoing\").")
+
+                wr.send_legal(board, uart, log=debug)
+                continue
+
+            next_move = chess.Move.null()
+
+            # Three distinct possible kinds of packets (in a sense) exist to prompt
+            # further action from the RPI:
+            # - RESTART: clear game state (rewind entire game)
+            # - HINT: send best move
+            # - UNDO: rewind game state one move at a time
+            match (wr.parse_button_event(next_packet)):
+                case ButtonEvent.RESTART:
+                    if debug:
+                        push_msg("Packet indicates RESTART.")
+ 
+                    with open(data_handle, 'w') as game_data:
+                        for game_move in board.move_stack:
+                            print(game_move.uci(), file=game_data)
+
+                    if debug:
+                        push_msg(f"(Attempted to) log game data in file \"{data_handle}\".")
+                        push_msg("Resetting board state...")
+
+                    wr.init_board(board, uart, log=debug)
+
+                    next_result = None
+
+                    while (next_result == None):
+                        try:
+                            next_result = sf.play(board, sf_limit)
+                        except TimeoutError:
+                            pass 
+
+                    best_move = next_result.move
+
+                    if debug:
+                        push_msg(f"Reset board state. Board now: TURN: {board.turn}, MOVES: {board.move_stack}")
+
+                    current_timestamp = datetime.now().isoformat()
+                    current_timestamp = current_timestamp.replace(':', '-')
+                    data_handle = f"/opt/chessboard/{current_timestamp}-game.txt"
+                    continue
+                case ButtonEvent.HINT:
+                    if debug:
+                        push_msg("Packet indicates HINT.")
+
+                    if (board.is_checkmate() or board.is_stalemate()): 
+                        continue
+
+                    encoded_best_move = wr.encode_packet(best_move, board, True)
+
+                    if debug:
+                        push_msg(f"Providing {encoded_best_move:08x} as best move.")
+
+                    uart.write(encoded_best_move.to_bytes(4, 'little'))
+                case ButtonEvent.UNDO:
+                    try:
+                        # Rewinds the board state---no further action needed.
+                        undone_move = board.pop()
+                        undone_move_packet = wr.encode_undo(undone_move, board)
+
+                        if debug:
+                            push_msg(f"Popped move: {undone_move.uci()}")
+                            if undone_move != None:
+                                push_msg(f"Sending undone move: 0x{undone_move_packet:08x}")
+
+                        next_result = None
+
+                        while (next_result == None):
+                            try:
+                                next_result = sf.play(board, sf_limit)
+                            except TimeoutError:
+                                pass
+
+                        best_move = next_result.move
+                        
+                        # Indicate to the MSP which specific move has been undone
+                        uart.write(undone_move_packet.to_bytes(4, 'little'))
+                    except IndexError:
+                        # IndexError innocuous; stack is empty, but not an error
+                        push_msg("Move stack empty--can't undo! Sending undo exhaust sentinel {wr.UNDO_EXHAUST:08x}.")
+                        uart.write(wr.UNDO_EXHAUST.to_bytes(4, 'little'))
+                    finally:
+                        # Log the updated move transcript
+                        with open(data_handle, 'w') as game_data:
+                            for game_move in board.move_stack:
+                                print(game_move.uci(), file=game_data)
+
+                        continue
+                case _:
+                    # Standard move. Add move to playing stack and update state
+                    next_move = wr.decode_packet(next_packet)
+
+                    if debug:
+                        push_msg("Packet is standard move.")
+                        push_msg(f"Decoded as move \"{next_move.uci()}.\"")
+
+                    board.push(next_move)
+
+                    with open(data_handle, 'w') as game_data:
+                        for game_move in board.move_stack:
+                            print(game_move.uci(), file=game_data)
+
+                    next_result = None
+
+                    while (next_result == None):
+                        try:
+                            next_result = sf.play(board, sf_limit)
+                        except TimeoutError:
+                            pass
+
+                    best_move = next_result.move
+
+                    if debug:
+                        if best_move:
+                            push_msg(f"Got best move {best_move.uci()} in response to new move.")
+                        else:
+                            push_msg(f"Best move is null. Checkmate or stalemate present.")
+
+                    # Check endgame conditions and indicate to MSP if present
+                    if (board.is_checkmate()):
+                        if debug:
+                            push_msg("ENDGAME: board indicates CHECKMATE!")
+
+                        uart.write(wr.CHECKMATE.to_bytes(4, 'little'))
+                    elif (board.is_stalemate() or board.has_insufficient_material(board.turn) or board.is_seventyfive_moves() or board.is_fivefold_repetition()):
+                        if debug:
+                            push_msg("ENDGAME: board indicates STALEMATE!")
+
+                        uart.write(wr.STALEMATE.to_bytes(4, 'little'))
+                    elif (board.is_check()):
+                        check_packet = wr.encode_check(board)
+                        
+                        if debug:
+                            push_msg(f"CHECK! Sending packet {hex(check_packet)}")
+
+                        uart.write(check_packet.to_bytes(4, 'little'))
+
+                    # Send all legal moves for the new board state
+                    wr.send_legal(board, uart, log=debug)
+
+            # ...then proceed to the next turn
+
+    sf.quit() # Code should never reach this---game loops forever.
+
+    if debug:
+        push_msg("CRITICAL ERROR: script exited!")
+
+
---
+++ wrapper/wrapper_util.py
@@ -0,0 +1,262 @@
+#!/usr/bin/env python3
+
+import chess
+import serial
+from enum import Enum
+
+# Various constants based on packet scheme. See
+# `/uart-protocol/uart_bidir_protocol.h` and `.c` for more information.
+SRC_FILE_SHIFT = 29
+SRC_RANK_SHIFT = 26
+DEST_FILE_SHIFT = 23
+DEST_RANK_SHIFT = 20
+PTYPE_SHIFT = 17
+
+M2_SHIFT = 16
+M2_SRC_FILE_SHIFT = 13
+M2_SRC_RANK_SHIFT = 10
+M2_DEST_FILE_SHIFT = 7
+M2_DEST_RANK_SHIFT = 4
+M2_PTYPE_SHIFT = 1
+
+MTYPE_SHIFT = 2
+MTYPE_NORMAL = 0
+MTYPE_PROMO = 1
+MTYPE_CAPTURE = 2
+MTYPE_CASTLE = 3
+
+# Special status constants to indicate exceptional game conditions (outcomes,
+# most often) for the MSP.
+MSP_SYN = 0x00000008
+SYNACK  = 0x00000001
+MSP_ACK = 0xFFFFFFFF
+
+MSP_REREQ       = 0x00000004
+UNDO_EXHAUST    = 0x00000008
+
+CHECK       = 0x00000011
+CHECKMATE   = 0x00000031
+STALEMATE   = 0x00000071
+
+
+class ButtonEvent(Enum):
+    NORMAL = 0
+    RESTART = 1
+    HINT = 2
+    UNDO = 3
+
+
+# Extracted here since the logic is more complex than a match-case.
+def encode_movetype(move: chess.Move, board: chess.Board) -> int:
+    global MTYPE_NORMAL
+    global MTYPE_PROMO
+    global MTYPE_CAPTURE
+    global MTYPE_CASTLE
+
+    if (move.promotion != None):
+        return MTYPE_PROMO
+    elif (board.is_capture(move)):
+        return MTYPE_CAPTURE
+    elif (board.is_castling(move)):
+        return MTYPE_CASTLE
+
+    return MTYPE_NORMAL
+
+
+def encode_packet(move: chess.Move, board: chess.Board, last_move: bool=False) -> int:
+    global SRC_FILE_SHIFT
+    global SRC_RANK_SHIFT
+    global DEST_FILE_SHIFT
+    global DEST_RANK_SHIFT
+    global PTYPE_SHIFT
+    global M2_SHIFT
+    global MTYPE_SHIFT
+
+    packet = 0
+
+    packet |= chess.square_file(move.from_square) << SRC_FILE_SHIFT
+    packet |= chess.square_rank(move.from_square) << SRC_RANK_SHIFT
+    packet |= chess.square_file(move.to_square) << DEST_FILE_SHIFT
+    packet |= chess.square_rank(move.to_square) << DEST_RANK_SHIFT
+
+    if (move.promotion):
+        packet |= (move.promotion << PTYPE_SHIFT)
+    else:
+        packet |= board.piece_type_at(move.from_square) << PTYPE_SHIFT
+
+    if (board.is_castling(move) or board.is_en_passant(move)):
+        packet |= (1 << M2_SHIFT)
+        if board.is_castling(move):
+            packet |= (1 << M2_PTYPE_SHIFT)
+            packet |= ((7*(board.turn==chess.BLACK)) << M2_SRC_RANK_SHIFT)
+            packet |= ((7*(board.turn==chess.BLACK)) << M2_DEST_RANK_SHIFT)
+            packet |= ((7*board.is_kingside_castling(move)) << M2_SRC_FILE_SHIFT)
+            packet |= ((3 + 2*board.is_kingside_castling(move)) << M2_DEST_FILE_SHIFT)
+        else:
+            packet |= (0 << M2_PTYPE_SHIFT)
+            packet |= (chess.square_file(move.to_square) << M2_SRC_FILE_SHIFT)
+            packet |= (chess.square_rank(move.from_square) << M2_SRC_RANK_SHIFT)
+            packet |= (chess.square_file(move.to_square) << M2_DEST_FILE_SHIFT)
+            packet |= (chess.square_rank(move.from_square) << M2_DEST_RANK_SHIFT)
+
+    packet |= encode_movetype(move, board) << MTYPE_SHIFT
+    packet |= (1 if last_move else 0)
+
+    return packet
+
+
+def encode_undo(move: chess.Move, board: chess.Board) -> int:
+    global SRC_FILE_SHIFT
+    global SRC_RANK_SHIFT
+    global DEST_FILE_SHIFT
+    global DEST_RANK_SHIFT
+    global PTYPE_SHIFT
+    global M2_SHIFT
+
+    packet = 0
+    packet |= chess.square_file(move.from_square) << SRC_FILE_SHIFT
+    packet |= chess.square_rank(move.from_square) << SRC_RANK_SHIFT
+
+    dest_file = chess.square_file(move.to_square)
+    packet |= dest_file << DEST_FILE_SHIFT
+    dest_rank = chess.square_rank(move.to_square)
+    packet |= dest_rank << DEST_RANK_SHIFT
+
+    packet |= board.piece_type_at(move.from_square) << PTYPE_SHIFT
+    packet |= (1 << M2_SHIFT) if (board.is_castling(move) or board.is_capture(move)) else (0 << M2_SHIFT) 
+
+
+    undone_ptype = None
+    undone_pcolor = 0
+
+    if board.is_en_passant(move):
+        undone_ptype = chess.PAWN # en passant captures can only capture pawns
+        dest_file = chess.square_file(move.to_square)
+        dest_rank = chess.square_rank(move.from_square)
+        packet |= (dest_file << M2_SRC_FILE_SHIFT) | (dest_file << M2_DEST_FILE_SHIFT)
+        packet |= (dest_rank << M2_SRC_RANK_SHIFT) | (dest_rank << M2_DEST_RANK_SHIFT)
+        undone_pcolor = (1 if board.color_at(move.from_square) == chess.WHITE else 0) << 3
+    elif (board.is_capture(move)):
+        undone_ptype = board.piece_type_at(move.to_square)
+        undone_pcolor = (1 if board.color_at(move.from_square) == chess.WHITE else 0) << 3
+        packet |= (dest_file << M2_SRC_FILE_SHIFT) | (dest_file << M2_DEST_FILE_SHIFT)
+        packet |= (dest_rank << M2_SRC_RANK_SHIFT) | (dest_rank << M2_DEST_RANK_SHIFT)
+    elif (board.is_castling(move)):
+        undone_pcolor = (1 if board.color_at(move.from_square) == chess.BLACK else 0) << 3
+        undone_ptype = chess.ROOK # The second piece type for castling can only be a rook
+        packet |= ((7*(board.turn==chess.BLACK)) << M2_SRC_RANK_SHIFT)
+        packet |= ((7*(board.turn==chess.BLACK)) << M2_DEST_RANK_SHIFT)
+        packet |= ((7*board.is_kingside_castling(move)) << M2_SRC_FILE_SHIFT)
+        packet |= ((3 + 2*board.is_kingside_castling(move)) << M2_DEST_FILE_SHIFT)
+
+    if undone_ptype != None:
+        packet |= undone_ptype
+
+    packet |= undone_pcolor
+
+    return packet
+
+
+def encode_check(board: chess.Board) -> int:
+    global MTYPE_SHIFT
+    global M2_DEST_FILE_SHIFT
+    global M2_DEST_RANK_SHIFT
+    global PTYPE_SHIFT
+
+    check_packet = 4
+
+    king_square = board.king(board.turn)
+    check_packet |= (chess.square_file(king_square) << M2_DEST_FILE_SHIFT)
+    check_packet |= (chess.square_rank(king_square) << M2_DEST_RANK_SHIFT)
+
+    return check_packet
+
+
+def decode_packet(packet: int) -> chess.Move:
+    global SRC_FILE_SHIFT
+    global SRC_RANK_SHIFT
+    global DEST_FILE_SHIFT
+    global DEST_RANK_SHIFT
+
+    move_str = ''
+
+    move_str += chess.FILE_NAMES[(packet >> SRC_FILE_SHIFT) & 0x7]
+    move_str += chess.RANK_NAMES[(packet >> SRC_RANK_SHIFT) & 0x7]
+    move_str += chess.FILE_NAMES[(packet >> DEST_FILE_SHIFT) & 0x7]
+    move_str += chess.RANK_NAMES[(packet >> DEST_RANK_SHIFT) & 0x7]
+
+    # if packet is tagged as promotion move
+    if ((packet >> MTYPE_SHIFT) & 0x3) == 1:
+        match ((packet >> PTYPE_SHIFT) & 0x7):
+            case 2:
+                move_str += 'n' # promoting to knight
+            case 3:
+                move_str += 'b' # promoting to bishop
+            case 4:
+                move_str += 'r' # promoting to rook
+            case 5:
+                move_str += 'q' # promoting to queen
+            case _:
+                pass # move string does not need to be appended
+
+    return chess.Move.from_uci(move_str)
+
+
+# A separate helper to detect exceptional button presses indicating special user
+# input on the chessboard.
+#
+# If restart/hint/undo requested, there's no move to decode within the packet.
+def parse_button_event(packet: int) -> ButtonEvent:
+    match (packet & 0x3):
+        case 0x1:
+            return ButtonEvent.RESTART
+        case 0x2:
+            return ButtonEvent.HINT
+        case 0x3:
+            return ButtonEvent.UNDO
+        case _:
+            return ButtonEvent.NORMAL
+
+
+def send_legal(board: chess.Board, shandle: serial.Serial, log: bool=True):
+    possible_moves = board.legal_moves
+    possible_count = possible_moves.count()
+
+    moves_dict = {}
+    move_src_square = ''
+
+    # Group moves into contiguous "chunks" based on source square
+    for move in possible_moves:
+        # .square_name method returns str (okay for dictionary key)
+        move_src_square = chess.square_name(move.from_square)
+        
+        # Categorize moves by source square.
+        # Offload this task to the RPI for easier move processing on the MSP.
+        if move_src_square not in moves_dict.keys():
+            moves_dict[move_src_square] = []
+
+        moves_dict[move_src_square].append(encode_packet(move, board))
+
+    packet_no = 0
+
+    for src_square in moves_dict.keys():
+        for packet in moves_dict[src_square]:
+            packet_no += 1
+
+            if packet_no == possible_count:
+                packet |= 1 # Set the packet's LSB to mark it as the last packet in series
+            
+            if log:
+                print(f"[DEBUG]: (wr.send_legal): sending packet 0x{packet:08X}")
+
+            shandle.write(packet.to_bytes(4, 'little'))
+
+
+def init_board(board: chess.Board, shandle: serial.Serial, log:bool=True):
+    # Clear playing stack and refresh list of valid moves
+    board.reset()
+    send_legal(board, shandle, log=log)
+
+
+if __name__ == '__main__':
+    pass
