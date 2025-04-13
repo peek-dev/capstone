920e837590b995dafde6d997404914453d142743
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2025-04-13 17:55:59 -0400

    Whoops, we switched the convention on pins again.

--- firmware/src/sensor.c
+++ firmware/src/sensor.c
@@ -47,22 +47,22 @@ TaskHandle_t xSensorTaskId = NULL;
 
 void prvSelectRow(uint8_t row) {
     assert(row < 8);
-    DL_GPIO_writePinsVal(MUX_GPIO_PIN_C_A0_PORT, MUX_GPIO_PIN_C_A0_PIN,
-                         MUX_GPIO_PIN_C_A0_PIN * !!(row & 1));
-    DL_GPIO_writePinsVal(MUX_GPIO_PIN_C_A1_PORT, MUX_GPIO_PIN_C_A1_PIN,
-                         MUX_GPIO_PIN_C_A1_PIN * !!(row & 2));
-    DL_GPIO_writePinsVal(MUX_GPIO_PIN_C_A2_PORT, MUX_GPIO_PIN_C_A2_PIN,
-                         MUX_GPIO_PIN_C_A2_PIN * !!(row & 4));
+    DL_GPIO_writePinsVal(MUX_GPIO_PIN_R_A0_PORT, MUX_GPIO_PIN_R_A0_PIN,
+                         MUX_GPIO_PIN_R_A0_PIN * !!(row & 1));
+    DL_GPIO_writePinsVal(MUX_GPIO_PIN_R_A1_PORT, MUX_GPIO_PIN_R_A1_PIN,
+                         MUX_GPIO_PIN_R_A1_PIN * !!(row & 2));
+    DL_GPIO_writePinsVal(MUX_GPIO_PIN_R_A2_PORT, MUX_GPIO_PIN_R_A2_PIN,
+                         MUX_GPIO_PIN_R_A2_PIN * !!(row & 4));
 }
 
 void prvSelectColumn(uint8_t column) {
     assert(column < 8);
-    DL_GPIO_writePinsVal(MUX_GPIO_PIN_R_A0_PORT, MUX_GPIO_PIN_R_A0_PIN,
-                         MUX_GPIO_PIN_R_A0_PIN * !!(column & 1));
-    DL_GPIO_writePinsVal(MUX_GPIO_PIN_R_A1_PORT, MUX_GPIO_PIN_R_A1_PIN,
-                         MUX_GPIO_PIN_R_A1_PIN * !!(column & 2));
-    DL_GPIO_writePinsVal(MUX_GPIO_PIN_R_A2_PORT, MUX_GPIO_PIN_R_A2_PIN,
-                         MUX_GPIO_PIN_R_A2_PIN * !!(column & 4));
+    DL_GPIO_writePinsVal(MUX_GPIO_PIN_C_A0_PORT, MUX_GPIO_PIN_C_A0_PIN,
+                         MUX_GPIO_PIN_C_A0_PIN * !!(column & 1));
+    DL_GPIO_writePinsVal(MUX_GPIO_PIN_C_A1_PORT, MUX_GPIO_PIN_C_A1_PIN,
+                         MUX_GPIO_PIN_C_A1_PIN * !!(column & 2));
+    DL_GPIO_writePinsVal(MUX_GPIO_PIN_C_A2_PORT, MUX_GPIO_PIN_C_A2_PIN,
+                         MUX_GPIO_PIN_C_A2_PIN * !!(column & 4));
 }
 
 uint16_t prvSingleADC(void) {
